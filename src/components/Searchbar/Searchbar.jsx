f7acef70df175c300f397473ea4c4f7a41282592
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-01-17 04:08:27 +0200

    Seachbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
+import { Component } from 'react';
+import { toast } from 'react-toastify';
+import { FcSearch } from 'react-icons/fc';
 
-const Searchbar = () => {
-  return (
-    <header class="searchbar">
-      <form class="form">
-        <button type="submit" class="button">
-          <span class="button-label">Search</span>
-        </button>
+import {
+  SearchFormInput,
+  SearchFormLabel,
+  SearchFormButton,
+  SearchForm,
+  SearchbarWrapper,
+} from './Seachbar.styled';
 
-        <input
-          class="input"
-          type="text"
-          autocomplete="off"
-          autofocus
-          placeholder="Search images and photos"
-        />
-      </form>
-    </header>
-  );
-};
+class Searchbar extends Component {
+  state = {
+    seachImage: '',
+  };
+  handleNameChange = event => {
+    this.setState({ seachImage: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.seachImage.trim() === '') {
+      return toast.error('Enter a search name');
+    }
+    this.props.onSubmit(this.state.seachImage);
+    this.setState({ seachImage: '' });
+  };
+  render() {
+    return (
+      <div>
+        <SearchbarWrapper>
+          <SearchForm onSubmit={this.handleSubmit}>
+            <SearchFormButton type="submit">
+              <FcSearch size={35} />
+              <SearchFormLabel>Search</SearchFormLabel>
+            </SearchFormButton>
+
+            <SearchFormInput
+              className="input"
+              type="text"
+              autoComplete="off"
+              autoFocus
+              onChange={this.handleNameChange}
+              value={this.state.seachImage}
+              placeholder="Search images and photos"
+            />
+          </SearchForm>
+        </SearchbarWrapper>
+      </div>
+    );
+  }
+}
 export default Searchbar;
