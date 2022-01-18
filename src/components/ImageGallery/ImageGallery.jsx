eadd8a522bea9f232efc870b138eecac19dd2b59
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-01-18 14:20:59 +0200

    ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,86 +1,33 @@
-import { Component } from 'react';
-import { BallTriangle } from 'react-loader-spinner';
-import Button from '../Button/Button';
+import PropTypes from 'prop-types';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
-import fetchHits from '../../services/api';
-import { ImageGalleryList, BackgroundImg } from './ImageGallery.styled';
-import { toast } from 'react-toastify';
-class ImageGallery extends Component {
-  state = {
-    images: [],
-    error: null,
-    status: 'idle',
-    page: 1,
-    showButton: false,
-  };
-  componentDidUpdate(prevProps, prevState) {
-    const prevName = prevProps.seachImage;
-    const nextName = this.props.seachImage;
-    if (prevName !== nextName) {
-      this.setState({ status: 'pending' });
-      fetchHits(nextName, this.state.page)
-        .then(hits => {
-          if (hits.hits.length === 0) {
-            toast.info('Nothing found for your request');
-          }
+import { ImageGalleryList } from './ImageGallery.styled';
 
-          this.setState({
-            images: hits.hits,
-            status: 'resolved',
-            showButton: true,
-          });
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
-  }
-  changeCurrentPage = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  render() {
-    const { images, error, status } = this.state;
-    if (status === 'idle') {
-      return <BackgroundImg />;
-    }
-    if (status === 'pending') {
-      return (
-        <BallTriangle
-          color="#3f51b5"
-          height={200}
-          width={200}
-          wrapperStyle={{
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column',
-          }}
+const ImageGallery = ({ images, openModal }) => {
+  return (
+    <ImageGalleryList>
+      {images.map(({ id, webformatURL, tags, largeImageURL }) => (
+        <ImageGalleryItem
+          key={id}
+          id={id}
+          webformatURL={webformatURL}
+          tags={tags}
+          largeImage={largeImageURL}
+          openModal={openModal}
         />
-      );
-    }
-    if (status === 'rejected') {
-      return <h1>{error.message}</h1>;
-    }
-    if (status === 'resolved') {
-      return (
-        <>
-          <ImageGalleryList>
-            {images.map(({ id, webformatURL, tags, largeImageURL }) => (
-              <ImageGalleryItem
-                key={id}
-                id={id}
-                webformatURL={webformatURL}
-                tags={tags}
-                largeImage={largeImageURL}
-                openModal={this.props.onClick}
-              />
-            ))}
-          </ImageGalleryList>
-          {this.state.showButton && <Button />}
-        </>
-      );
-    }
-  }
-}
+      ))}
+    </ImageGalleryList>
+  );
+};
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      tags: PropTypes.string.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      openModal: PropTypes.func,
+      largeImage: PropTypes.string,
+    }).isRequired,
+  ).isRequired,
+};
 
 export default ImageGallery;
