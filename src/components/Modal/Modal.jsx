0824290da1f7324541f7ad84ee504d248e06a8e9
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-01-18 14:21:34 +0200

    Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
 import { Overlay, ModalWrapper } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
@@ -8,17 +9,14 @@ class Modal extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
   }
-
   componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeyDown);
   }
-
   handleKeyDown = event => {
     if (event.code === 'Escape') {
       this.props.onClose();
     }
   };
-
   handleBackdropClick = event => {
     if (event.currentTarget === event.target) {
       this.props.onClose();
@@ -33,4 +31,7 @@ class Modal extends Component {
     );
   }
 }
+Modal.propTypes = {
+  onClick: PropTypes.func,
+};
 export default Modal;
