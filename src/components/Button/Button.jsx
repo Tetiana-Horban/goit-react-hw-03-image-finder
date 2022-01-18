bebd3496c486f744c7be4c11eaf91f10aaedea06
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-01-18 14:20:45 +0200

    Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,19 @@
 import ButtonLoader from './Button.styled';
+import PropTypes from 'prop-types';
 
-const Button = () => {
-  return <ButtonLoader type="button">Load more</ButtonLoader>;
+const Button = ({ loadMore, onClick }) => {
+  async function LoadMoreImg() {
+    await onClick();
+    loadMore();
+  }
+  return (
+    <ButtonLoader type="button" onClick={LoadMoreImg}>
+      Load more
+    </ButtonLoader>
+  );
+};
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  loadMore: PropTypes.func.isRequired,
 };
 export default Button;
