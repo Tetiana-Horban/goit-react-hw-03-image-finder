e60f2db9735fdf60f21a2645a74d1bb935d471d9
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-01-18 14:21:52 +0200

    fetch

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const fetchHits = (name, page) => {
   const API_KEY = '24373442-b431678aac0bac18598ec6531';
   return fetch(
-    `https://pixabay.com/api/?q=${name}&${page}=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+    `https://pixabay.com/api/?q=${name}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
   ).then(response => {
     if (response.ok) {
       return response.json();
